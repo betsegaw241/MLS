5d8c763046986c5d171f0292e4f459a17de3b186
TolasaGanati <tolog766@gmail.com>
2024-03-25 11:33:31 +0300

    order page little change

--- src/app/Pages/OrderPage/index.ts
+++ src/app/Pages/OrderPage/index.ts
@@ -1,90 +1,26 @@
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useYourPageSlice } from "./slice";
-// import {
-//   selectYourData,
-//   selectIsLoading,
-//   selectPage,
-//   selectLimit,
-//   selectTotalPages,
-// } from "./slice/selectors";
-// import {
-//   IHandleOnChange,
-//   IHandleLoanAmountChange,
-// } from "app/components/YourComponent/types";
-// import { useNavigate, useParams } from "react-router";
+import OrderComponent from "app/Components/OrderComponent";
+import { useEffect, useState } from "react";
 
-// // Your imports and dependencies go here
+function OrderPage() {
+  const [orders, setOrders] = useState([]);
 
-// export const YourPage = () => {
-//   const dispatch = useDispatch();
-//   const { actions } = useYourPageSlice();
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch("/");
+        const data = await response.json();
+        setOrders(data);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    };
 
-//   // Selecting data from Redux state
-//   const yourData = useSelector(selectYourData);
-//   const isLoading = useSelector(selectIsLoading);
-//   const page = useSelector(selectPage);
-//   const limit = useSelector(selectLimit);
-//   const totalPages = useSelector(selectTotalPages);
+    fetchOrders();
+  }, []);
 
-//   // Local state for managing filters
-//   const [filterValues, setFilterValues] = useState<any>({});
+  return (
+    <OrderComponent orders={orders} />
+  );
+}
 
-//   // React Router integration
-//   const { pageNo } = useParams();
-//   const navigate = useNavigate();
-
-//   // Event handlers
-//   const handlePaginationChange = (
-//     event: ChangeEvent<unknown>,
-//     value: number
-//   ) => {
-//     navigate(`/your-route/${value}`);
-//   };
-
-//   const handleFilterChange = ({ queryName, value, event }: IHandleOnChange) => {
-//     // Your filter change logic
-//   };
-
-//   const handleLoanAmountChange = ({
-//     min,
-//     max,
-//     queryName,
-//   }: IHandleLoanAmountChange) => {
-
-//   };
-
-//   const handleOnReset = () => {
-//     // Your reset logic
-//   };
-
-//   // Effect hooks
-//   useEffect(() => {
-//     if (pageNo) {
-//       dispatch(actions.setPageState(Number(pageNo)));
-//     } else {
-//       dispatch(actions.setPageState(1));
-//     }
-//   }, [pageNo]);
-
-//   useEffect(() => {
-//     dispatch(actions.fetchYourData({ page, limit }));
-//   }, [page, limit]);
-
-//   return (
-//     <>
-//       {/* YourComponent is a placeholder for the component responsible for rendering your data */}
-//       <YourComponent
-//         filterValues={filterValues}
-//         handleFilterChange={handleFilterChange}
-//         handleLoanAmountChange={handleLoanAmountChange}
-//         handleOnReset={handleOnReset}
-//         handlePaginationChange={handlePaginationChange}
-//         isLoading={isLoading}
-//         yourData={yourData}
-//         page={page}
-//         totalPages={totalPages}
-//       />
-//     </>
-//   );
-// };
+export default OrderPage;
